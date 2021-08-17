d95180475d9252d7d3174223d01a474e6c8cf973
dwikoadya <dwikoadya@gmail.com>
2021-08-17 16:48:27 +0700

    Done

---
+++ client/src/actions/index.js
@@ -0,0 +1,43 @@
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
+import Swal from 'sweetalert2'
+
+const API_URL = `http://localhost:3001/graphql`
+
+const client = new ApolloClient({
+  uri: API_URL
+})
+
+export const loadContactSuccess = ({ result, item }) => ({
+  type: 'LOAD_CONTACT_SUCCESS',
+  result,
+  item
+})
+
+export const loadContactFailure = () => ({
+  type: 'LOAD_USER_FAILURE'
+})
+
+export const loadContact = () => {
+  const contactQuery = gql`
+  query {
+    users {
+      id
+      Name
+      Phone
+    }
+  }`
+  return dispatch => {
+    return client.query({
+      query: contactQuery,
+    })
+    .then(function (response) {
+      console.log(response)
+      dispatch(loadContactSuccess(response.data.users))
+    })
+    .catch(function (error) {
+      console.log(error)
+      dispatch(loadContactFailure())
+    })
+  }
+}
